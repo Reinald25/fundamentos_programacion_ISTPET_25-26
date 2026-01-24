392d7453752ae8a2b2edc0a996a6f94efa04a1e9
Reinald25 <ronaldsubia6@gmail.com>
2026-01-24 01:17:59 -0500

    24/01/2026

--- Gestion_de_clientes/src/gestion_de_cliente/Entrada_de_clientes.java
+++ Gestion_de_clientes/src/gestion_de_cliente/Entrada_de_clientes.java
@@ -1,40 +1,63 @@
 package gestion_de_cliente;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Entrada_de_clientes {
     //clase scanner como instnacia global
+	static final Integer c = 3;
     static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
+		
 		datos();
+		sc.close();
 	}
+
 public static void datos() {
 	System.out.println("Ingrese numero de datos a almacenar");
 	//se genera el numero de filas de la matriz
-	Integer n = sc.nextInt();
-	//se genera una fila de nombres
-	String[] nombre = new String[n];
-	//fila de cedula
-	String[] cedula = new String[n];
-	//fila de correos
-	String[] correo = new String[n];
-	    for(int i = 0;i<n;i++) {
-	    //se toman los datos de las filas
-    	System.out.println("Ingrese nombre");
-    	nombre[i]=sc.next();
-    	System.out.println("Ingrese cedula");
-    	cedula[i]=sc.next(); 
-    	System.out.println("Ingrese correo");
-    	correo[i]=sc.next();
-    	
-    	}
-	//se imprimen las filas en orden
-    System.out.println(Arrays.toString(nombre));
-    System.out.println(Arrays.toString(cedula));
-    System.out.println(Arrays.toString(correo));
-
-
+	Integer f = sc.nextInt();
+	//la matriz toma las filas de la consola y genra 3 columnas para almacenar
+	//3 datos
+	String[][] datos = new String[f][c];
+	//se generan filas por medio de f o numero de datos a almacenar
+	for (int i = 0; i < f; i++) { 
+		//se generan las columnas para los 3 datos a tomar
+	    for (int j = 0; j < c; j++) { 
+	    	//compara si el indice de j e imprime un mensaje especifico
+	    	//y almacena la informacion en rango i j
+	    	if(j==0) {
+	    		System.out.print("Ingrese nombre: " );
+	        datos[i][j] = sc.next();
+	    	}
+	    	if(j==1) {
+	    		System.out.print("Ingrese cedula: " );
+	        datos[i][j] = sc.next();
+	    	}
+	    	if(j==2) {
+	    		System.out.print("Ingrese correo: " );
+	        datos[i][j] = sc.next();
+	    	}
+	        
+	    }
+}
+	//impresion con salto
+	for (int i = 0; i < f; i++) {
+	    for (int j = 0; j < c; j++) {
+	    	//evalua e imprime el elemento de la matriz
+	    	if(j==0) {
+	    		System.out.print("Nombre: "+datos[i][j] + " ");
+	    	}
+	    	if(j==1) {
+	    		System.out.print("Cedula: "+datos[i][j] + " ");
+	    	}
+	    	if(j==2) {
+	    		System.out.print("Correo: "+datos[i][j] + " ");
+	    	}
+	        
+	    }
+	    //slato de linea
+	    System.out.println(); 
+	}
 }
 
 }
--- Gestion_de_clientes/src/gestion_de_cliente/test.java
+++ Gestion_de_clientes/src/gestion_de_cliente/test.java
@@ -1,19 +1,21 @@
 package gestion_de_cliente;
 
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 
 public class test {
 
 	public static void main(String[] args) {
-		String[] arr1 = {"1", "2"};
-        String[] arr2 = {"3", "4"};
-
-        // Concatenar usando Streams [8]
-        String[] resultado = Stream.concat(Arrays.stream(arr1), Arrays.stream(arr2))
-                                   .toArray(String[]::new);
-
-        System.out.println(Arrays.toString(resultado));
-	}
-
+		Scanner sc = new Scanner(System.in);
+		int[][] matriz = {{1, 2}, {3, 4}};
+		for (int i = 0; i < matriz.length; i++) {
+		    for (int j = 0; j < matriz[i].length; j++) {
+		        System.out.print(matriz[i][j] + " "); // Imprime horizontal
+		    }
+		    System.out.println(); // Salto de línea después de cada fila [13]
+		}
+}
 }
